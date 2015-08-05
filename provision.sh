4f9adcafa62a9b5a3bfd6a220eec5c5dac8a6a85
Rutu Mulkar-Mehta <rutu@moz.com>
2015-08-05 14:33:58 -0700

    travis build config

---
+++ provision.sh
@@ -0,0 +1,22 @@
+# die on error
+set -e
+
+sudo apt-get update
+
+sudo apt-get -y install libatlas-base-dev libatlas-dev lib{blas,lapack}-dev gfortran
+sudo pip install conda
+conda_deps='pip numpy scipy'
+conda create -p $HOME/py --yes $conda_deps "python=$TRAVIS_PYTHON_VERSION"
+export PATH=$HOME/py/bin:$PATH
+pip install -r requirements.txt
+python setup.py build_ext --inplace
+
+(
+    cd ~/git
+    git clone https://github.com/seomoz/vocab.git
+    (
+        cd ~/git/vocab
+        pip install -r requirements.txt
+        python setup.py install
+    )
+)
