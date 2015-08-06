5a17fbcf06e135defaaa7337b4bc41de36b8acad
Rutu Mulkar-Mehta <rutu@moz.com>
2015-08-06 11:31:28 -0700

    updating vpath

--- provision.sh
+++ provision.sh
@@ -7,7 +7,7 @@ sudo apt-get -y install libatlas-base-dev libatlas-dev lib{blas,lapack}-dev gfor
 sudo pip install conda
 conda_deps='pip numpy scipy'
 conda create -p $HOME/py --yes $conda_deps "python=$TRAVIS_PYTHON_VERSION"
-pip install cython>=0.21.1
+pip install -r requirements.txt
 
 
 mkdir ~/git
@@ -21,4 +21,4 @@ mkdir ~/git
     )
 )
 python setup.py build_ext --inplace
-export PATH=$HOME/py/lib:$HOME/py/bin:$PATH
+export PATH=$HOME/py/bin:$PATH
